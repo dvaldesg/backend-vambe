9512f27266c8bb9538e2ebb415747d21ae4d42b6
dvaldesg <diego.valdes.g@uc.cl>
2025-07-16 02:09:11 -0400

    feat(guards & decorators): added get-users

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -2,22 +2,26 @@ import { PassportStrategy } from "@nestjs/passport";
 import { ExtractJwt, Strategy } from "passport-jwt";
 import { Injectable } from "@nestjs/common";
 import { ConfigService } from "@nestjs/config";
+import { PrismaService } from "src/prisma/prisma.service";
 
 interface JwtPayload {
-    sub: string;
+    sub: number;
     email: string;
 }
 
 @Injectable()
-export class JwtStrategy extends PassportStrategy(Strategy) {
+export class JwtStrategy extends PassportStrategy(Strategy, 'jwt') {
 
-    constructor(config: ConfigService) {
+    constructor(
+        config: ConfigService,
+        private prisma: PrismaService,
+    ) {
 
         const jwtSecret = config.get<string>('JWT_SECRET');
         if (!jwtSecret) {
             throw new Error('JWT_SECRET is not defined in configuration');
         }
-        
+
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             secretOrKey: jwtSecret,
@@ -25,6 +29,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     }
 
     async validate(payload: JwtPayload) {
+        const user = await this.prisma.user.findUnique({
+            where: { id: payload.sub },
+        });
+
+        if (!user) {
+            throw new Error('User not found');
+        }
+
         return { userId: payload.sub, email: payload.email };
     }
 }

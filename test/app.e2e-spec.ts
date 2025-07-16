a045b8fc88cf174e827540f16da2faaaf0416e24
dvaldesg <diego.valdes.g@uc.cl>
2025-07-16 12:43:35 -0400

    feat(prisma): cleanDb for testing

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,24 +1,36 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { PrismaService } from '../src/prisma/prisma.service';
 
 describe('AppModule (e2e)', () => {
+
   let app: INestApplication;
+  let prisma: PrismaService;
+
   beforeAll(async () => {
+
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
     }).compile();
 
     app = moduleRef.createNestApplication();
+
     app.useGlobalPipes(
       new ValidationPipe({
         whitelist: true,
       }),
     );
+
     await app.init();
+
+    prisma = app.get<PrismaService>(PrismaService);
+    await prisma.cleanDb();
   });
+
   afterAll(() => {
     app.close();
   });
+  
   it.todo('should pass');
 });

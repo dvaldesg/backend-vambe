16969f6bbc430c1188274d84937eb6ba7ddba551
dvaldesg <diego.valdes.g@uc.cl>
2025-07-23 01:38:37 -0400

    delete localhost from prod xd

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   
   app.enableCors({
-    origin: ['http://localhost:3000', 'https://frontend-vambe.vercel.app'],
+    origin: ['https://frontend-vambe.vercel.app'],
     credentials: true,
   });
   
